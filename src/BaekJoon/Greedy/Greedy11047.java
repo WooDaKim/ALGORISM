d713949cba150bcb609ebc64ed8540e8aeab6dd9
wooda <woosskimgit@gmail.com>
2022-07-19 23:03:48 +0900

    백준 11047문제

--- src/BaekJoon/Greedy/Greedy11047.java
+++ src/BaekJoon/Greedy/Greedy11047.java
@@ -1,5 +1,6 @@
 package BaekJoon.Greedy;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Greedy11047 {
@@ -20,46 +21,28 @@ public class Greedy11047 {
         //입력 받기
         Scanner sc = new Scanner(System.in);
 
-        // N = 종류   K = 합
+        // N = 종류   K = 합  count = 최솟값
         int N = sc.nextInt();
         int K = sc.nextInt();
-
-        // coin 배열에 N개의 배열만큼 만든다.
-        int[] coin = new int[N];
-
-        for(int i=0 ; i < N ; i++) {
-            //각 배열의 값을 받아넣는다.
-            coin[i] = sc.nextInt();
-        }
-
-        // 코인의 최솟값
         int count = 0;
 
-        //동전이 오름차순으로 입력이 주어지기때문에 반복문을 i = n-1로 시작하고(배열의 인덱스 시작이 0); i가 양수일때 까지만; 역행으로 계산해주자.
-        for(int i=N-1 ; i >= 0; i--) {
+        // 동전의 종류를 받을 배열 만들기 개수는 종류의 수만큼
+        int[] coinType = new int[N];
 
-            if(coin[i] <= K) {  //K 값보다 (입력받은 총량) coin인덱스 값이 작다면 실행하자
-                // 최솟값 = 최솟값 + (합 / 배열)
-                count = count + (K / coin[i]);
-                // 합 % 배열 = 합
-                K = K % coin[i];
-            }
-
+        for(int i = 0 ; i < N ; i++) {
+            // 배열에 숫자넣기
+            coinType[i] = sc.nextInt();
         }
 
+        for(int i = N-1 ; i>=0 ; i--) {
+            if(coinType[i] <= K) {  //K 보다 작거나 같을때
+                count += (K / coinType[i]);     // 이미 if문에서 조건이 걸렸기때문에 무조건 K는 배수가 된다.
+                K %= coinType[i]; // 계산하고 나머지값
+            }
+        }
         System.out.println(count);
 
+
+
     }
-
-    /**
-     * 풀이
-     *
-     * 동전의 최솟값을 구하는 과정
-     * i) 가장 큰 동전으로 내서 개수를 줄일것인가
-     * ii) 배수의 동전이 있는가?
-     *
-     */
-
-
-
 }
